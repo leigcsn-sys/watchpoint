d4334d765cea64e4cc3e70b380cc23790ac8c87e
leigcsn-sys <jlgacusana@gmail.com>
2026-08-12 18:05:39 +0800

    Add watches resource routes and history route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WatchController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('watches', WatchController::class);
+    Route::get('watches/{watch}/history', [WatchController::class, 'history'])->name('watches.history');
 });
 
 require __DIR__.'/auth.php';
